069cfed10bff7ca94f8e4c07a3781edafae22e3f
RemagOfficial <lloydfb456@gmail.com>
2024-06-24 04:06:00 +0100

    switched to datagen for item models and added microcontroller recipes

---
+++ src/main/java/com/remag/pncepcb/datagen/DataGenerators.java
@@ -0,0 +1,22 @@
+package com.remag.pncepcb.datagen;
+
+import com.remag.pncepcb.PNCExtraPCBs;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod.EventBusSubscriber(modid = PNCExtraPCBs.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
+    }
+}
